0d30be9848879f61e85bc203ea277743711c1674
Rafael Tadeu <rafael.tadeu@twistsystems.com>
2025-09-03 03:15:35 -0300

    remove unused setup_logging function from main module

--- tsp_genetic/__main__.py
+++ tsp_genetic/__main__.py
@@ -3,16 +3,9 @@ import logging
 import logging.config
 
 
-def setup_logging():
-    # File from environment
-    # Setup with dictConfig
-    pass
-
-
 def main():
     print("Hello World")
 
 
 if __name__ == "__main__":
-    setup_logging()
     main()
